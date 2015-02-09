0e86b4281408df1bd3090c1f5ff3e9c076bcb3f5
Mischa Hondius <mhondius@gmail.com>
2015-02-09 19:52:52 +0100

    klaar met rode balletjes

--- app/src/main/java/com/example/equifilm/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/equifilm/listviewexample/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.equifilm.listviewexample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +17,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] favoriteTVShows = {"Pushing Daisies", "Better Off Ted", "Twin Peaks", "Freaks and Geeks", "Orpgan Black", "Walking Dad",
+        "Breaking Bad", "The 400", "Alphas", "Life On Mars", "Breaking Bad", "The 400", "Alphas", "Life On Mars", "Breaking Bad", "The 400", "Alphas", "Life On Mars"};
+
+        ListAdapter theAdapter = new MyAdapter(this, favoriteTVShows);
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+        theListView.setAdapter(theAdapter);
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                                    @Override
+                                                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                                                        String tvShowPicked = "You selected " +
+                                                                String.valueOf(adapterView.getItemAtPosition(position));
+                                                        Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+
     }
 
 
